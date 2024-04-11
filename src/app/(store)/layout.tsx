667bd6a014e29e0a96473109ffece6ac5cf4eaad
lorenasg1 <lorenasg1@gmail.com>
2024-04-10 21:37:49 -0300

    feat: add header component

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from 'react'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
 	return (
-		<div>
+		<div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-home gap-5 p-8">
 			<Header />
 			{children}
 		</div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    fontFamily: {
-      sans: ["var(--font-inter)", "sans-serif"],
-    },
-  },
-  plugins: [],
-};
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		fontFamily: {
+			sans: ['var(--font-inter)', 'sans-serif'],
+		},
+		gridTemplateRows: {
+			home: 'min-content max-content',
+		},
+	},
+	plugins: [],
+}
 
-export default config;
+export default config
