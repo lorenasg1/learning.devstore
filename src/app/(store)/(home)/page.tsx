2d5694f00bf73053e20a8b153823380c11223dae
lorenasg1 <lorenasg1@gmail.com>
2024-04-12 18:01:21 -0300

    feat: add product page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -26,7 +26,7 @@ export default async function Home() {
 	return (
 		<div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
 			<Link
-				href={`/products/${highlightedProduct.slug}`}
+				href={`/product/${highlightedProduct.slug}`}
 				className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
 			>
 				<Image
@@ -52,7 +52,7 @@ export default async function Home() {
 			{otherProducts?.map((product) => (
 				<Link
 					key={product.slug}
-					href={product.slug}
+					href={`/product/${product.slug}`}
 					className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
 				>
 					<Image
