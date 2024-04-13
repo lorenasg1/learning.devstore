9c838e4d736e6033a9bb6627f7d46ed45b67aa66
lorenasg1 <lorenasg1@gmail.com>
2024-04-13 11:57:04 -0300

    feat: product page and route

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,12 +1,42 @@
+import { api } from '@/data/api'
+import type { Product as TProduct } from '@/data/types/product'
 import Image from 'next/image'
 
-export default function Product() {
+type ProductProps = {
+	params: {
+		slug: string
+	}
+}
+
+async function getProduct(slug: string): Promise<TProduct> {
+	const response = await api(`/products/${slug}`, {
+		next: {
+			revalidate: 60 * 60, // 1h,
+		},
+	})
+
+	const product = await response.json()
+
+	return product
+}
+
+export default async function Product({ params: { slug } }: ProductProps) {
+	const product = await getProduct(slug)
+	const price = new Intl.NumberFormat('pt-BR', {
+		style: 'currency',
+		currency: 'BRL',
+	}).format(product?.price)
+	const pricePart = new Intl.NumberFormat('pt-BR', {
+		style: 'currency',
+		currency: 'BRL',
+	}).format(product?.price / 12)
+
 	return (
 		<div className="relative grid max-h-[860px] grid-cols-3">
 			<div className="col-span-2 overflow-hidden">
 				<Image
-					src={'/moletom-never-stop-learning.png'}
-					alt=""
+					src={product.image}
+					alt={product.description}
 					width={1000}
 					height={1000}
 					quality={100}
@@ -15,20 +45,18 @@ export default function Product() {
 			</div>
 
 			<div className="flex flex-col justify-center px-12">
-				<h1 className="text-3xl font-bold leading-tight">
-					Moletom Never Stop Learning
-				</h1>
+				<h1 className="text-3xl font-bold leading-tight">{product.title}</h1>
 
 				<p className="mt-2 leading-relaxed text-zinc-400">
-					Moletom fabricado com 80% de algodão e 12% de poliéster.
+					{product.description}
 				</p>
 
 				<div className="mt-8 flex items-center gap-3">
 					<span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
-						R$129
+						{price}
 					</span>
 					<span className="text-small text-zinc-400">
-						Em 12x sem juros de R$10,75
+						Em 12x sem juros de {pricePart}
 					</span>
 				</div>
 
