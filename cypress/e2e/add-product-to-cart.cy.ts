07018d089e2ce9b94130565464441119db9e3361
lorenasg1 <lorenasg1@gmail.com>
2024-04-17 09:05:36 -0300

    test: add to product to cart

---
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -0,0 +1,34 @@
+describe('add product to cart', () => {
+	it('should be able to navigate to product page and add it to the cart', () => {
+		cy.visit('http://localhost:3000')
+		cy.get('a[href^="/product"]').first().click()
+		cy.location('pathname').should('include', '/product')
+
+		cy.contains('adicionar à sacola', { matchCase: false }).click()
+
+		cy.contains('Sacola (1)').should('exist')
+	})
+
+	it('should not add duplicated products to cart', () => {
+		cy.visit('http://localhost:3000')
+		cy.get('a[href^="/product"]').first().click()
+		cy.location('pathname').should('include', '/product')
+		cy.contains('adicionar à sacola', { matchCase: false }).click()
+		cy.contains('adicionar à sacola', { matchCase: false }).click()
+
+		cy.contains('Sacola (1)').should('exist')
+	})
+
+	it('should not add duplicated products to cart', () => {
+		cy.visit('http://localhost:3000')
+
+		cy.get('input[name="q"]').type('moletom').parent('form').submit()
+		cy.location('pathname').should('include', '/search')
+
+		cy.get('a[href^="/product"]').first().click()
+		cy.contains('adicionar à sacola', { matchCase: false }).click()
+
+		cy.location('pathname').should('include', '/product')
+		cy.contains('Sacola (1)').should('exist')
+	})
+})
